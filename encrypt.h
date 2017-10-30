c9f143310e6fafc0d26c063938a867061dbb5fc5
tig3rninja14 <tig3rninja14@outlook.com>
2017-10-30 18:26:52 -0500

    Created Files

---
+++ encrypt.h
@@ -0,0 +1,13 @@
+//Programmers: Evan Maynard & Justin Palermo        Date: 10/30/2017
+//Class: CS1570                                     Section: E
+//File: encrypt.h
+//Purpose: To decrypt a file based on a set encryption.
+
+#ifndef "encrypt_h"
+#define "encrypt_h"
+
+
+
+
+
+#endif
