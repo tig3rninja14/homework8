1f91a98347b05bfb2d0094779526dcebb06527bd
tig3rninja14 <tig3rninja14@outlook.com>
2017-10-30 18:30:07 -0500

    added headers

---
+++ encrypt_functions.cpp
@@ -0,0 +1,4 @@
+//Programmers: Evan Maynard & Justin Palermo        Date: 10/30/2017
+//Class: CS1570                                     Section: E
+//File: encrypt_functions.cpp
+//Purpose: To decrypt a file based on a set encryption.
